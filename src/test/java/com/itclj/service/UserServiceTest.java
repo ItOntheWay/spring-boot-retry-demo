9ea7ff98ad06f3cc4092b343844222db813f558e
lujun.chen <lujun.chen@msxf.com>
2017-08-14 09:36:18 +0800

    添加单元测试

---
+++ src/test/java/com/itclj/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package com.itclj.service;
+
+import javax.annotation.Resource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Create by lujun.chen on 2017/08/14
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+  private Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
+
+  @Resource
+  private UserService userService;
+
+  @Test
+  public void getUser() {
+    String result = userService.getUser(1222);
+    logger.info(result);
+  }
+
+}
